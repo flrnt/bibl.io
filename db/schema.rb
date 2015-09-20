a1ecba07896aa6f1ede535489a26b795fa77e445
Florent Allain <allain.florent@gmail.com>
2015-09-20 23:10:36 +0200

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,63 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150920205456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "authors", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.string   "year"
+    t.integer  "category_id"
+    t.text     "description"
+    t.integer  "author_id"
+    t.integer  "quantity"
+    t.boolean  "available"
+    t.string   "isbn"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "books", ["author_id"], name: "index_books_on_author_id", using: :btree
+  add_index "books", ["category_id"], name: "index_books_on_category_id", using: :btree
+
+  create_table "borrows", force: :cascade do |t|
+    t.integer  "book_id"
+    t.integer  "reader_id"
+    t.datetime "date_in"
+    t.datetime "date_out"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "borrows", ["book_id"], name: "index_borrows_on_book_id", using: :btree
+  add_index "borrows", ["reader_id"], name: "index_borrows_on_reader_id", using: :btree
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "readers", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "phone_number"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_foreign_key "books", "authors"
+  add_foreign_key "books", "categories"
+  add_foreign_key "borrows", "books"
+  add_foreign_key "borrows", "readers"
 end

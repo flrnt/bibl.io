2c309aa62a0c4643ff49ae198f0519b93441664d
Florent Allain <allain.florent@gmail.com>
2015-09-20 23:43:45 +0200

    add CRUD to books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,33 @@
 class BooksController < ApplicationController
-  def index
 
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(:id)
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_book
+  end
+
+  def book_params
   end
 end

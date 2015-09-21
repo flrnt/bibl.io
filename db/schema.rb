54e68a835512fce80339f8af5f8e4d3dba76972f
Florent Allain <allain.florent@gmail.com>
2015-09-21 19:54:04 +0200

    remove available from books

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921161843) do
+ActiveRecord::Schema.define(version: 20150921175236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 20150921161843) do
     t.text     "description"
     t.integer  "author_id"
     t.integer  "quantity"
-    t.boolean  "available"
     t.string   "isbn"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
